05bf3607bdeefd51ee7936e231187d926eadd05b
Jason Wolfe <jason@w01fe.com>
2017-01-07 18:00:17 -0800

    Add pingpong

---
+++ pingpong.ino
@@ -0,0 +1,21 @@
+// set symmetrically
+
+void pingpong() {
+  int dir = 1;
+  int row = 0;
+  int reps = 10;
+  int colors[] = {WHITE, RED};
+  for(int step = 0; step < 100000; step++) {
+    int period = ledsPerStrip / reps;
+    colorWipe(BLACK);
+    for(int i = 0; i < reps; i++) {      
+      pixel(row, (i * period + step) % ledsPerStrip, colors[0]);
+      pixel((5 - row), (i * period + step + period/2) % ledsPerStrip, colors[1]);
+    }
+    row += dir;
+    if (row == 0 || row == nStrips - 1) dir *= -1;
+    leds.show();
+    delayMicroseconds(50000);
+  }
+}
+
