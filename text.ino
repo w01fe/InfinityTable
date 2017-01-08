0dc46e1a92caf0c3660828abba1bfe69453091c5
Sarah Reed <sereed82@gmail.com>
2017-01-07 20:52:29 -0800

    Cleaning up initial code

--- text.ino
+++ text.ino
@@ -5,14 +5,14 @@ char font[] = {
 };
 
 void drawText(const char s[]) {
-  for(int i = 0; i < leds.numPixels(); i++) leds.setPixel(i, 0);
+  colorWipe(BLACK);
   int p = 0;
   for(const char *c = s; *c; c++) {
     for(int i = 0; i < 5; i++) {
-      char row = font[*c - 'a' + i];
+      char column = font[*c - 'a' + i];
       for(int j = 0; j < 5; j++) {
-        if (row % 2 == 1) leds.setPixel((i + 1) * ledsPerStrip + p + 4 - j, 0xFF0000);
-        row /= 2;
+        if (column % 2 == 1) pixel(i + 1, p + 4 - j + 90, RED);
+        column /= 2;
       }
     }
     p += 6;
