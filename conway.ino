ae9edc3319bd9b6cd165578a3b9d5748f2c67304
Ian Davis <jungziege@gmail.com>
2017-01-09 23:20:34 -0800

    Add conway's game of life

--- conway.ino
+++ conway.ino
@@ -2,35 +2,29 @@ boolean isAlive(int row, int col) {
   return !(lastPixel(row,col) == BLACK);
 }
 
-int countCell(int row, int col) {
-  if (isAlive(row, col)) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-
 int neighbors[nStrips][ledsPerStrip];
 
 int numNeighborsHelper(int row, int col) {
   int totalCount = 0;
   for (int r = -1; r <= 1; r++) {
     for (int c = -1; c <= 1; c++) {
-      if (!(c == 0 && r == 0)) {
-        if (isAlive(wrap(row + r, nStrips), 
+      if (isAlive(wrap(row + r, nStrips),
                     wrap(col + c, ledsPerStrip))) {
-          totalCount += 1;
-        }
+        totalCount += 1;
       }
     }
   }
-  return totalCount;
+  if (isAlive(row,col)) {
+    return totalCount-1;
+  } else {
+    return totalCount;
+  }
 }
 
 void countNeighbors() {
   for (int row=0; row < nStrips; row++) {
     for (int col = 0; col < ledsPerStrip; col++) {
-      neighbors[row][col] = numNeighbors(row,col);
+      neighbors[row][col] = numNeighborsHelper(row,col);
     }
   }
 }
@@ -39,45 +33,55 @@ int numNeighbors(int row, int col) {
   return neighbors[row][col];
 }
 
-void evolveCell(int row, int col) {
+void evolveCell(int row, int col, int color) {
   int n = numNeighbors(row, col);
   if (isAlive(row,col)) {
     if (n == 2 || n == 3) {
-      pixel(row,col,DIM_WHITE);
+      pixel(row,col,color);
     } else {
       pixel(row,col,BLACK);
     }
   } else {
     if (n == 3) {
-      pixel(row,col,DIM_WHITE);
+      pixel(row,col,color);
     } else {
       pixel(row,col,BLACK);
     }
   }
 }
 
-void evolve () {
+void evolve (int color) {
   for (int row=0; row < nStrips; row++) {
     for (int col = 0; col < ledsPerStrip; col++) {
-      evolveCell(row,col);
+      evolveCell(row,col, color);
     }
   }
 }
 
-void setupGlider() {
-  pixel(0,ledsPerStrip/2+1,DIM_WHITE);
-  pixel(1,ledsPerStrip/2+2,DIM_WHITE);
-  pixel(2,ledsPerStrip/2+0,DIM_WHITE);
-  pixel(2,ledsPerStrip/2+1,DIM_WHITE);
-  pixel(2,ledsPerStrip/2+2,DIM_WHITE);
+void setupGlider(int color) {
+  pixel(0,ledsPerStrip/2+1,color);
+  pixel(1,ledsPerStrip/2+2,color);
+  pixel(2,ledsPerStrip/2+0,color);
+  pixel(2,ledsPerStrip/2+1,color);
+  pixel(2,ledsPerStrip/2+2,color);
 }
 
-void conway() {
-  setupGlider();
+void randomSetup(int color) {
+  for (int row = 0; row < nStrips; row++) {
+    for (int col = 0; col < ledsPerStrip; col++) {
+      if (random(12) == 2) {
+        pixel(row, col, color);
+      }
+    }
+  }
+}
+
+void conway(int color) {
+  randomSetup(color);
   for (int i = 0; i< 1000000; i++) {
     leds.show();
-    delayMicroseconds(2000000);
+    delayMicroseconds(20000);
     countNeighbors();
-    evolve();
+    evolve(color);
   }
 }
